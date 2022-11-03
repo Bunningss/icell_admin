fd1fb38baae23de6ec2c2e3560110f7b201e4a82
Bunnings <mahmudovi183@gmail.com>
2022-11-03 17:44:08 +0600

    login page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -2,7 +2,15 @@ import './Login.scss';
 
 const Login = () => {
   return (
-    <div className='login'>Login page</div>
+    <div className='login'>
+      <div className="wrapper">
+        <form action="" className="form">
+          <input type="text" placeholder='Enter Your Username' className='form-input'/>
+          <input type="text" placeholder='Enter Your Password' className='form-input'/>
+          <button className="button">Login</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
